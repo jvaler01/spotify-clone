f68cd62384be75f02120b052809ec088d90a600c
jvaler01 <jorgevm13@gmail.com>
2023-10-16 21:26:31 +0200

    playlist route by param

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -9,9 +9,18 @@ export const routes: Routes = [
       { path: 'home',
         loadComponent: () => import('./pages/home-page/home-page.component').then( m => m.HomePageComponent ) 
       },
-      { path: 'playlist',
-        loadComponent: () => import('./pages/playlist-page/playlist-page.component').then( m => m.PlaylistPageComponent )
+      { path: 'search',
+        loadComponent: () => import('./pages/search-page/search-page.component').then( m => m.SearchPageComponent )
       },
+      { path: 'playlist/:id',
+        loadComponent: () => import('./pages/playlist-page/playlist-page.component').then( m => m.PlaylistPageComponent )
+      }
+      /* { path: 'playlist',
+        children: [
+          { path: ':id',loadComponent: () => import('./pages/playlist-page/playlist-page.component').then( m => m.PlaylistPageComponent )},
+          { path: '', redirectTo: 'home', pathMatch: 'full' },
+        ]
+      } */,
       { path: '**', redirectTo: 'home' }
     ]
   }
---
+++ src/app/dashboard/pages/search-page/search-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-page.component.html',
+  styleUrls: ['./search-page.component.scss']
+})
+export class SearchPageComponent {
+
+}
