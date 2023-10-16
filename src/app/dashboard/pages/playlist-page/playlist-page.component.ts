c2325afd4be746cc540a246dc5718dd95e151bc0
jvaler01 <jorgevm13@gmail.com>
2023-10-16 23:08:26 +0200

    get param from route in playlist

--- src/app/dashboard/pages/playlist-page/playlist-page.component.ts
+++ src/app/dashboard/pages/playlist-page/playlist-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-playlist-page',
@@ -8,6 +10,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './playlist-page.component.html',
   styleUrls: ['./playlist-page.component.scss']
 })
-export class PlaylistPageComponent {
-
+export class PlaylistPageComponent implements OnInit, OnDestroy {
+  id: string = '0';
+  private routeSub: Subscription = new Subscription;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    })
+  }
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
 }
